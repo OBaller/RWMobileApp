ee33e5ae69b4c9ad66d1eb53e5214bbeead20c46
brightsummit.co <naseemoyebola@gmail.com>
2022-02-21 10:34:17 +0100

    InitialCommit

---
+++ RWMobileApp/RWMobileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RWMobileApp
+//
+//  Created by naseem on 21/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
